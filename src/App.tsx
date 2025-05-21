cce7fa5e9df14aa22998e0248793e91c556769bb
latifah president <latifah.pres@gmail.com>
2025-05-20 20:02:52 -0400

    update file structure remove outdate react scripts install more recent version of react use vite install local dev helpers

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './assets/stylesheets/app.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <p className="text-3xl text-red-500 underline">HELLO WORLD</p>
+      <p className="text-3xl font-bold underline">CHARTS AND GRAPHS</p>
+    </div>
+  );
+};
+
+export default App;
