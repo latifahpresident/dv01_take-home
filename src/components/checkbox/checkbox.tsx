3352cd5a12685e2c58289b48d412ef95073f6fef
latifah president <latifah.pres@gmail.com>
2025-05-21 07:01:56 -0400

    add specs

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -18,9 +18,9 @@ export const Checkbox = ({ checked, onCheckedChange = () => {}, label }: Checkbo
   };
 
   return (
-    <div className="flex items-center gap-2" onClick={handleClick}>
+    <div data-testid="checkbox" className="flex items-center gap-2" onClick={handleClick}>
       <div className="h-6 w-6 flex items-center justify-center border border-gray-300 rounded-md p-0.5">
-        {isChecked && <CheckIcon />}
+        {isChecked && <CheckIcon data-testid="checkbox-icon" />}
       </div>
       {label && (
         <span className="text-base capitalize text-gray-700 font-medium">
---
+++ src/components/multiselect/multiselect.test.tsx
@@ -0,0 +1,49 @@
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Multiselect } from './multiselect';
+
+describe('Multiselect Component', () => {
+  const options = [
+    { label: 'Option 1', value: 'opt1' },
+    { label: 'Option 2', value: 'opt2' },
+    { label: 'Option 3', value: 'opt3' },
+  ];
+
+  beforeEach(() => {
+    render(<Multiselect options={options} placeholder="dropdown textbox" />);
+  });
+
+  test('renders placeholder when no option selected', () => {
+    const textbox = screen.getByText('dropdown textbox');
+
+    expect(textbox).toBeInTheDocument();
+  });
+
+  test('opens options list on click', () => {
+    const textbox = screen.getByText('dropdown textbox');
+    fireEvent.click(textbox);
+
+    expect(screen.getByText('option 1')).toBeVisible();
+    expect(screen.getByText('option 2')).toBeVisible();
+    expect(screen.getByText('option 3')).toBeVisible();
+  });
+
+  test('selects and deselects options and displays count', async () => {
+    const textbox = screen.getByText('dropdown textbox');
+    await userEvent.click(textbox);
+    screen.debug();
+
+    // Select Option 1
+    const opt1Checkbox = screen.getByText('option 1');
+    fireEvent.click(opt1Checkbox);
+
+    expect(screen.getByText('1 Applied')).toBeInTheDocument();
+    expect(screen.getByTestId('checkbox-icon')).toBeInTheDocument();
+
+    const opt2Checkbox = screen.getByText('option 2');
+    fireEvent.click(opt2Checkbox);
+
+    expect(screen.getByText('2 Applied')).toBeInTheDocument();
+  });
+});
